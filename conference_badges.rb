82ba7ba88288a00d48e493e550b157045089989a
Amee Covarrubias <ameecova@yahoo.com>
2018-07-28 01:57:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+# Write your code here
+require 'pry'
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  @maker = attendees.map {|person| badge_maker(person)}
+  return @maker
+end
+
+
+def assign_rooms(attendees)
+  @assign = attendees.map.with_index {|person, index| "Hello, #{person}! You'll be assigned to room #{index + 1}!"}
+  return @assign
+end 
+
+def printer(attendees)
+  #binding.pry
+  puts @maker[0]
+  puts @assign[0]
+end 
